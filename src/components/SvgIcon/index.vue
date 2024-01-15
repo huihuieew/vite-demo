5ab469170567012fd550900b2e85436f7ebd602a
huihuieew <1142317178@qq.com>
2024-01-15 21:52:27 +0800

    chore: 引入svg图标

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div>svg icon</div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import App from "./App.vue";
 import router from "@/router/index";
 import pinia from "./store/main";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import SvgIcon from "@/components/SvgIcon/index.vue";
 
 const app = createApp(App);
 Object.keys(ElementPlusIconsVue).forEach(key => {
     app.component(key, ElementPlusIconsVue[key as keyof typeof ElementPlusIconsVue]);
 })
 
-app.use(pinia).use(router).mount("#app");
+app.use(pinia).use(router).component('svg-icon', SvgIcon).mount("#app");
